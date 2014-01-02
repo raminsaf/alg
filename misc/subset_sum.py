aa22232210880375a97508df8e5a997bb3d87676
Ramin Safaie <raminsaf@gmail.com>
2014-01-01 20:08:09 -0800

    Fixed infinite recursion.

--- misc/subset_sum.py
+++ misc/subset_sum.py
@@ -1,14 +1,17 @@
 
+solutions = {}
 theSet = [8, 6, 7, 5, 3, 10, 9]
 theGoalSum = 15
 
 def subsetSum(theSet, theGoalSum, theSum, theSubset):
-  print theSet, theSum, theSubset
+  #print theSet, theSum, theSubset
   if theSum == theGoalSum:
-    print theSubset
+    if solutions.has_key(str(theSubset)) is False:
+      solutions[str(theSubset)] = 1
+      print theSubset
   if theSum < theGoalSum and 0 != len(theSet):
     for i in range(len(theSet)):
-      nextSet = theSet[:i] + theSet[i:]
+      nextSet = theSet[:i] + theSet[i+1:]
       nextSubset = theSubset[:]
       subsetSum(nextSet, theGoalSum, theSum, nextSubset)
       nextSubset.append(theSet[i])
