eca17f91574a8b2344cf5a26b22906d54fb3307c
Ramin Safaie <raminsaf@gmail.com>
2014-01-01 20:20:09 -0800

    Corrected!

--- misc/subset_sum.py
+++ misc/subset_sum.py
@@ -1,20 +1,17 @@
 
-solutions = {}
 theSet = [8, 6, 7, 5, 3, 10, 9]
 theGoalSum = 15
 
 def subsetSum(theSet, theGoalSum, theSum, theSubset):
   #print theSet, theSum, theSubset
   if theSum == theGoalSum:
-    if solutions.has_key(str(theSubset)) is False:
-      solutions[str(theSubset)] = 1
-      print theSubset
+    print theSubset
   if theSum < theGoalSum and 0 != len(theSet):
-    for i in range(len(theSet)):
-      nextSet = theSet[:i] + theSet[i+1:]
-      nextSubset = theSubset[:]
-      subsetSum(nextSet, theGoalSum, theSum, nextSubset)
-      nextSubset.append(theSet[i])
-      subsetSum(nextSet, theGoalSum, theSum + theSet[i], nextSubset)
+    first = theSet[0]
+    nextSet = theSet[1:]
+    nextSubset = theSubset[:]
+    subsetSum(nextSet, theGoalSum, theSum, nextSubset)
+    nextSubset.append(first)
+    subsetSum(nextSet, theGoalSum, theSum + first, nextSubset)
 
 subsetSum(theSet, theGoalSum, 0, [])
